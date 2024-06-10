5706b1aaa659fa82ba5b0eda197fc46d91c87d2a
Anna-J-o-s-e <annajose941@gmail.com>
2024-06-10 15:35:47 +0530

    api created

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+
+const app=express()
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+app.get("/contact",(req,res)=>{
+    res.send("Welcome to my contacts page")
+})
+app.listen(8080,()=>{
+    console.log("server started")
+})
